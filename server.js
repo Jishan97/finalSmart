fbb072805d41406f3e01a1a15a39f55293dc95bf
jishan97 <shaikh.jishan1997@gmail.com>
2019-01-11 23:04:18 +0530

    added env process

--- server.js
+++ server.js
@@ -236,8 +236,7 @@ app.post('/new', upload.single('image'), async (req, res) => {
 })
 
 
-
-const PORT = 7777
+const PORT = process.env.PORT || 7777
 app.listen(PORT, () => {
   console.log(`Server is running ${PORT}`)
 })
